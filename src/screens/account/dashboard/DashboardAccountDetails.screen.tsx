8d03e95cdef9e33373901f591851bf5dead5e898
Bahti rek <bahtirek@gmail.com>
2026-03-09 21:51:48 -0400

    created dashboard screens

---
+++ src/screens/account/dashboard/DashboardAccountDetails.screen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const DashboardAccountDetailsScreen = () => {
+  return (
+    <View>
+      <Text>DashboardAccountDetailsScreen</Text>
+    </View>
+  )
+}
+
+export default DashboardAccountDetailsScreen
+
+const styles = StyleSheet.create({})
