114b0e8155b51e7a8b63296fd17e486f509bfdbb
Bahti rek <bahtirek@gmail.com>
2026-03-16 19:33:17 -0400

    adding spacing when keyboard is on screen

--- src/screens/cart/EditCartItemScreen.tsx
+++ src/screens/cart/EditCartItemScreen.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { CartStackParamList } from '@/navigation/navigation-types';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
 import PurchaseDetails from '@/components/common/PurchaseDetails';
+import { KeyboardAvoidingView, Platform } from 'react-native';
+import { useCartStore } from '@/stores/cart.store';
 
 
 type Props = NativeStackScreenProps<CartStackParamList, 'EditCartItem'>;
@@ -11,15 +12,23 @@ type Props = NativeStackScreenProps<CartStackParamList, 'EditCartItem'>;
 const EditCartItemScreen = ({route}: Props) => {
   const { cartItem } = route.params;
   const navigation = useNavigation();
+  const [showHeader, setShowHeader] = useState(true)
+  const addItemToEdit = useCartStore(state => state.addItemToEdit);
 
   const handleButtonPress = () => {
+    addItemToEdit({})
     navigation.goBack();
   }
 
+
+
   return (
-    <SafeAreaView edges={["left", "right"]} style={{flex: 1, backgroundColor: '#FFFFFF'}}>
+     <KeyboardAvoidingView
+          behavior='padding'
+          keyboardVerticalOffset={Platform.OS === "ios" ? 100 : 0} 
+          contentContainerStyle={{flexGrow: 1}}>
       <PurchaseDetails handleButtonPress={handleButtonPress} cartItemToEdit={cartItem} buttonLabel='Update'/>
-    </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 }
 
